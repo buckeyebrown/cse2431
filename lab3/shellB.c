9f8b2a4bbeccac1b573e8f041718419cbc3327d6
buckeyebrown <brown.4414@osu.edu>
2017-04-21 15:57:21 -0400

    prog

--- lab3/shellB.c
+++ lab3/shellB.c
@@ -19,6 +19,65 @@ int numberOfCommands;
 
 void addToHistory(int pos, char* inputBuff);
 void historyCommand();
+void setup(char inputBuff[], char *args[],int *background, FILE* fp);
+void exitProgram(FILE *fp);
+void loadHistory(FILE *fp);
+void saveHistory(FILE *fp);
+
+int main(void)
+{
+    char inputBuff[MAXLINE]; /* Input buffer  to hold the command entered */
+    char *args[MAXLINE/2+1]; /* Command line arguments */
+    char historyBufferForFile[MAXCMDHISTORY * MAXLINE];
+    int background;         /* Equals 1 if a command is followed by '&', else 0 */
+    numberOfCommands = 0;
+
+    int a = 0;
+    while (a < MAXCMDHISTORY){
+        memset(commandHistory[a], '\0', sizeof(commandHistory[a]));
+        a++;
+    }
+    memset(inputBuff, '\0', sizeof(inputBuff));
+
+    FILE* fp = fopen("commandhistory.txt", "ab+"); //Open File
+    loadHistory(fp);
+
+    while (1){            /* Program terminates normally inside setup */
+
+	pid_t pid;
+	char **command_args_ptr = &args[1];
+	background = 0;
+
+	printf("CSE2431Sh$ ");  /* Shell prompt */
+        fflush(0);
+
+        setup(inputBuff, args, &background, fp);       /* Get next command */
+
+    if ((strncmp(inputBuff, "history", 7) == 0) || (strncmp(inputBuff, "h", 1) == 0)){
+        historyCommand();
+    }
+    else if ((strncmp(inputBuff, "exit", 4) == 0) || (strstr(inputBuff, "exit") != NULL)){
+        exitProgram(fp);
+    }
+    
+            /* Fill in the code for these steps:  
+         (1) Fork a child process using fork(),
+         (2) The child process will invoke execvp(),
+         (3) If bkgnd == 0, the parent will wait; 
+            otherwise returns to the setup() function. */
+        pid = fork();
+            if (pid == 0) {
+                //printf("**\n%s\n", args[0]);
+                //printf("$$\n%s\n", args);
+                execvp(args[0], args);
+            }
+            else {
+                if (background == 0) wait(pid);
+            }        
+    }
+
+
+}
 
 /** The setup() routine reads in the next command line string storing it in the input buffer.
 The line is separated into distinct tokens using whitespace as delimiters.  Setup also 
@@ -41,13 +100,14 @@ void setup(char inputBuff[], char *args[],int *background, FILE* fp)
     start = -1;
 
     if (length == 0){
-        saveHistory(fp);
-        exit(0);
+        exitProgram(fp);
     }
 
     if (length < 0){
         perror("error reading command");
-	exit(-1);           /* Terminate with error code of -1 */
+        saveHistory(fp);
+        fclose(fp);
+        exit(-1);           /* Terminate with error code of -1 */
     }
     
     if (strncmp(inputBuff, "rr", 2) == 0){
@@ -108,38 +168,24 @@ void setup(char inputBuff[], char *args[],int *background, FILE* fp)
         default :             /* Some other character */
             if (start == -1)
                 start = i;
-	}
+    }
  
     }    
     args[j] = NULL; /* Just in case the input line was > 80 */
     
 } 
 
-void exitProgram(FILE *fp)
-{
-    saveHistory(fp);
-    fclose(fp); //Close file pointer
-    exit(0);
+void saveHistory(FILE *fp){
+
+    fwrite(commandHistory, sizeof(char), sizeof(commandHistory), fp);
+    printf("History written to file.\n");
 }
 
 void loadHistory(FILE *fp){
-    /**
-    int x = 0;
-    char buffer[MAXLINE];
-    while(!feof(fp)){
-        strcpy(buffer, "\0");
-
-        fgets(buffer, MAXLINE, fp);
-        if (strcmp(buffer, "\0") != 0){
-            strcpy(commandHistory[x], buffer);
-            printf("%s\n", commandHistory);
-        }
-        x++;
-    }
-    */
     fread(commandHistory, sizeof(char), sizeof(commandHistory), fp);
     printf("History loaded.\n");
     rewind(fp);
+    //count the new lines for each command
     char c;
     while(!feof(fp)){
         c = fgetc(fp);
@@ -149,76 +195,6 @@ void loadHistory(FILE *fp){
             }
     }
     rewind(fp);
-}
-
-void saveHistory(FILE *fp){
-    /**
-    int x = 0;
-    char buffer[MAXLINE];
-    while (x < MAXCMDHISTORY){
-        strcpy(buffer, "\0");
-        strcpy(buffer, commandHistory[x]);
-        if(strcmp(buffer,"\0") != 0){        
-            fprintf(fp, "%s", buffer);
-        }
-        x++;
-    }*/
-    fwrite(commandHistory, sizeof(char), sizeof(commandHistory), fp);
-    printf("History written to file.\n");
-}
-
-int main(void)
-{
-    char inputBuff[MAXLINE]; /* Input buffer  to hold the command entered */
-    char *args[MAXLINE/2+1]; /* Command line arguments */
-    char historyBufferForFile[MAXCMDHISTORY * MAXLINE];
-    int background;         /* Equals 1 if a command is followed by '&', else 0 */
-    numberOfCommands = 0;
-
-    int a = 0;
-    while (a < MAXCMDHISTORY){
-        memset(commandHistory[a], '\0', sizeof(commandHistory[a]));
-        a++;
-    }
-    memset(inputBuff, '\0', sizeof(inputBuff));
-
-    FILE* fp = fopen("commandhistory.txt", "ab+"); //Open File
-    loadHistory(fp);
-
-    while (1){            /* Program terminates normally inside setup */
-
-	pid_t pid;
-	char **command_args_ptr = &args[1];
-	background = 0;
-
-	printf("CSE2431Sh$ ");  /* Shell prompt */
-        fflush(0);
-
-        setup(inputBuff, args, &background, fp);       /* Get next command */
-
-    if ((strncmp(inputBuff, "history", 7) == 0) || (strncmp(inputBuff, "h", 1) == 0)){
-        historyCommand();
-    }
-    else if ((strncmp(inputBuff, "exit", 4) == 0) || (strstr(inputBuff, "exit") != NULL)){
-        exitProgram(fp);
-    }
-    
-            /* Fill in the code for these steps:  
-         (1) Fork a child process using fork(),
-         (2) The child process will invoke execvp(),
-         (3) If bkgnd == 0, the parent will wait; 
-            otherwise returns to the setup() function. */
-        pid = fork();
-            if (pid == 0) {
-                //printf("**\n%s\n", args[0]);
-                //printf("$$\n%s\n", args);
-                execvp(args[0], args);
-            }
-            else {
-                if (background == 0) wait(pid);
-            }        
-    }
-
 }
 
 void addToHistory(int pos, char* inputBuff){
@@ -248,4 +224,11 @@ void historyCommand(){
         printf("\n");
         a++;
     }
+}
+
+void exitProgram(FILE *fp)
+{
+    saveHistory(fp);
+    fclose(fp); //Close file pointer
+    exit(0);
 }
